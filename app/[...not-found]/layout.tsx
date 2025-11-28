5105febb0785ec7d3dc517d4291426437c43ab07
Karen Misaghian <157001468+KarenMisaghian@users.noreply.github.com>
2025-11-28 20:57:08 +0330

    Revert "feat(theme): add the main layout and not found page"

--- app/[...not-found]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFoundLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/src/components/QueryProvider";
-import { ThemeProvider } from "@/src/components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          <QueryProvider>
-            {children}
-          </QueryProvider>
-        </ThemeProvider>
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
